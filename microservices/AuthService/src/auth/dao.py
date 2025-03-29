bfc2515940e1dcb5516fcbab6c6d86ab175a32ba
Ilya Kozlobrodov <kozlobrodov.ilya@gmail.com>
2025-03-29 23:11:30 +0700

    refactor dao

--- microservices/AuthService/src/auth/dao.py
+++ microservices/AuthService/src/auth/dao.py
@@ -1,32 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from auth.models import User
-from sqlalchemy import select, update, delete
+from dao import BaseDAO
 
 
-class UserDAO:
+class UserDAO(BaseDAO[User]):
     model = User
-
-    def __init__(self, session: AsyncSession):
-        self._session = session
-
-    async def find_one_or_none(self, id: int) -> User | None:
-        stmt = select(self.model).filter(self.model.id == id)
-
-        query = await self._session.execute(stmt)
-        return query.scalar_one_or_none()
-
-    async def find_many(self, **filter_by) -> list[User]:
-        stmt = select(self.model).filter_by(**filter_by)
-
-        query = await self._session.execute(stmt)
-        return query.scalars().all()
-
-    async def update_one(self, id: int, data: dict) -> User:
-        stmt = update(self.model).filter(self.model.id == id).values(**data)
-
-        await self._session.execute(stmt)
-
-    async def delete_one(self, id: int) -> None:
-        stmt = delete(self.model).filter(self.model.id == id)
-
-        await self._session.execute(stmt)
--- microservices/AuthService/src/dao.py
+++ microservices/AuthService/src/dao.py
@@ -1,7 +1,6 @@
-from typing import List, TypeVar, Generic, Type
+from typing import TypeVar, Generic, Type
 from sqlalchemy.ext.asyncio import AsyncSession
-from auth.models import User
-from sqlalchemy import select, update, delete
+from sqlalchemy import select, update, delete, insert
 from database import BaseModel
 
 T = TypeVar("T", bound=BaseModel)
@@ -16,24 +15,24 @@ class BaseDAO(Generic[T]):
         if self.model is None:
             raise ValueError("Model not implemented.")
 
-    async def find_one_or_none(self, data_id: int) -> User | None:
-        stmt = select(self.model).filter(self.model.id == data_id)
+    async def create(self, data: dict):
+        stmt = insert(self.model).values(data)
+        await self._session.execute(stmt)
 
+    async def find_one_or_none(self, data_id: int):
+        stmt = select(self.model).filter(self.model.id == data_id)
         query = await self._session.execute(stmt)
         return query.scalar_one_or_none()
 
-    async def find_many(self, **filter_by) -> list[User]:
+    async def find_many(self, **filter_by):
         stmt = select(self.model).filter_by(**filter_by)
-
         query = await self._session.execute(stmt)
         return query.scalars().all()
 
-    async def update_one(self, data_id: int, data: dict) -> User:
+    async def update_one(self, data_id: int, data: dict):
         stmt = update(self.model).filter(self.model.id == data_id).values(**data)
-
         await self._session.execute(stmt)
 
-    async def delete_one(self, data_id: int) -> None:
+    async def delete_one(self, data_id: int):
         stmt = delete(self.model).filter(self.model.id == data_id)
-
         await self._session.execute(stmt)
